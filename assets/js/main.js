3a575cb6851b2703375c3887d20fc853ca9030e4
Lee <abe_jiawenl@iastate.edu>
2019-09-05 01:08:53 -0500

    project page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,8 +98,8 @@
 		// Lightbox gallery.
 			$window.on('load', function() {
 
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
+				$('.two').poptrox({
+					caption: function($a) { return $a.next('q').text(); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
 					popupCloserText: '',
